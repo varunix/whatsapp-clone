c7849f4e3b5b5bbdec17cad3547327d5cb667eee
Varun Chaubey <varunchaubey4@gmail.com>
2022-12-07 04:42:59 +0530

    Add addUser api

---
+++ server/controller/user-controller.js
@@ -0,0 +1,19 @@
+import User from "../model/User.js";
+
+
+export const addUser = async (req, res) => {
+    try {
+       let findUser = await User.find({ sub: req.body.sub });
+
+       if(findUser) {
+           res.status(200).json({ msg: 'User already exists' });
+           return;
+       }
+
+       const newUser = new User(req.body);
+       await newUser.save();
+       return res.status(200).json(newUser);
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+}
